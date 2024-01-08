33ed720ebd27931ce43a56f4f7efb8d04070139f
SolightzZ <94944606+SolightzZ@users.noreply.github.com>
2024-01-09 00:34:58 +0700

    Add files via upload

---
+++ s.mjs
@@ -0,0 +1,4 @@
+// Add this event listener for Discord button
+document.getElementById("discordJoin").addEventListener("click", () => {
+    window.location.href = "https://discord.gg/gtqfbmvTJK "; // Replace with your OAuth2 URL
+});
